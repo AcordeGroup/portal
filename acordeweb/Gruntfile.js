d84b55cb4ec6e7fc4db1fb35c41f15aed479114a
Carlos Moh <carlosmoh@gmail.com>
2015-10-28 02:30:50 -0430

    Se actualizan los archivos fuentes:
    Gruntfile.js - se completo para que realizara de forma exitosa la construcción del sitio
    package.json - se actualizo para incluir el paquete JADE
    acorde.less - se modificó para que buscará en forma relativa las imagenes de home y contacto
    index.html - se modificó para buscar las versiones minificadas de los archivos

--- acordeweb/Gruntfile.js
+++ acordeweb/Gruntfile.js
@@ -1,23 +1,69 @@
+//ESTA ES LA FUNCION CONTENEDORA - Contiene todo nuestro código GRUNT
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/js/<%= pkg.name %>.js',
-        dest: 'build/js/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig(
+    //Este es el objeto config que define cada una de las tareas a ejecutar
+    {
+        pkg: grunt.file.readJSON('package.json'),
+        clean: ["build"],
+        uglify: {
+            options: {
+                //Comentario que aparece en el encabezado del archivo mini de salida
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            destino: {
+                files: {
+                    'build/js/acorde.min.js': ['src/js/acorde.js'],
+                    'build/js/bootstrap.min.js': ['src/js/bootstrap.js'],
+                    'build/js/snap.svg.min.js': ['src/js/snap.svg.js']
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: 
+                [
+                    {'build/index.html':'src/index.html'}
+                ]
+            },
+            jquery: {
+                files: [{
+                    expand: true, flatten: true, filter: 'isFile',
+                    src: ['bower_components/jquery/dist/jquery.min*js'],
+                    dest: 'build/js'
+                }]
+            },
+            css: {
+                files: 
+                [
+                    {'build/css/acorde.less': ['src/css/acorde.less']},
+                    {'build/css/bootstrap.css': ['src/css/bootstrap.css']},
+                    {'build/css/bootstrap-theme.css': ['src/css/bootstrap-theme.css']},                    
+                ]
+            },
+            "font-awesome": {
+                files:
+                [
+                    {'build/css/font-awesome.min.css': ['src/font-awesome/css/font-awesome.min.css']},
+                    {expand: true, flatten: true, src: ['src/font-awesome/fonts/**'], dest: 'build/fonts', filter: 'isFile'}
+                ]
+            },
+            images: {
+                files: 
+                [
+                    {expand: true, cwd: 'src/', src: ['images/**'], dest: 'build/', filter: 'isFile'}
+                ]
+                    
+            }
+        }
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+    //CARGA LOS PLUGINS
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    //SE REGISTRA LAS TAREAS
+    grunt.registerTask('default', ['clean','copy','uglify']);
 
 };
